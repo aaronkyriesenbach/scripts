0dcb5561fb2d78cd0eecd4a0614859903594a209
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2021-03-24 09:24:41 -0400

    Remove retry functionality to simplify iadl

--- iadownload/File.py
+++ iadownload/File.py
@@ -1,22 +1,20 @@
 class File:
-    def __init__(self, url, metadata, supported_extensions):
-        self.metadata = metadata
+    def __init__(self, url, metadata):
         self.url = url
         self.ia_base_name = url[url.rindex("/"):]
+        self.ext = self.ia_base_name[self.ia_base_name.rindex(".") + 1:]
         self.md5 = metadata.get("md5")
-
-        self.supported_extensions = supported_extensions
+        self.title = metadata.get("title")
         self.track_num = metadata.get("track")
 
     def __str__(self):
-        return f"{self.url}: {self.get_base_name()}"
+        return f"{self.url}: {self.get_file_name()}"
 
     def get_track_num(self):
-        return str(self.track_num).zfill(2) if self.track_num else self.track_num
+        return str(self.track_num).zfill(2) if self.track_num and int(self.track_num) < 10 else self.track_num
 
     def get_title(self):
-        return self.metadata.get("title").replace("/", "_") if self.metadata.get(
-            "title") else f"Track {self.get_track_num()}"
+        return self.title.replace("/", "_") if self.title else f"Track {self.get_track_num()}"
     
-    def get_base_name(self):
-        return f"{self.get_track_num()} - {self.get_title()}"
+    def get_file_name(self):
+        return f"{self.get_track_num()} - {self.get_title()}.{self.ext}"
